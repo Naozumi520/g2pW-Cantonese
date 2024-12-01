e4a344fbd482f468633e6af9e636c72a6dea1332
Naozumi <naozumi0512@gmail.com>
2024-12-02 01:30:59 +0800

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -1,7 +1,8 @@
+# root = './20241128-rimePure-zjyg/'
 root = './rimeExtract_dataset/'
 
 manual_seed = 1313
-model_source = './bert-large-cantonese'
+model_source = './bert-base-cantonese'
 polyphonic_chars_path = root + 'POLYPHONIC_CHARS.txt'
 window_size = 32
 num_workers = 2
@@ -15,7 +16,7 @@ param_conditional = {
 }
 
 # for training
-exp_name = 'rimeDataLowerLR_BERT_L_DescWS-Sec-cLin-B_POSw01'
+exp_name = '20241201_BERT_B_DescWS-Sec-cLin-B_POSw01'
 train_sent_path = root + 'train.sent'
 train_lb_path = root + 'train.lb'
 valid_sent_path = root + 'dev.sent'
@@ -23,10 +24,9 @@ valid_lb_path = root + 'dev.lb'
 test_sent_path = root + 'test.sent'
 test_lb_path = root + 'test.lb'
 batch_size = 128
-# lr = 5e-5
-lr = 2e-5
+lr = 5e-5
 val_interval = 200
-num_iter = 30000
+num_iter = 13000
 use_pos = True
 param_pos = {
     'weight': 0.1,
